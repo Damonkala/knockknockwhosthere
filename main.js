dcdde3824f050bc7178f23c2fb7991f6c5a58822
Richard Gautereaux <Richieg72@gmail.com>
2015-11-07 01:11:21 -0800

    Tessel working ambient and wifi in main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("Test");
+
+var tessel = require('tessel')
+var wifiInit = require('./wifi/wifi-control.js')
+var ambientFiles = require('./ambient/ambient.js')
+
+wifiInit.wifiInit();
+
+ambientFiles.ambientInit();
