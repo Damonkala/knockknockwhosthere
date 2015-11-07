6e425760b31817d7192e5d21f8832cbd3d72f199
Rachel Wood <r@codinghouse.co>
2015-11-07 13:19:52 -0800

    Add new styles and socket set-up

--- frontend/main.js
+++ frontend/main.js
@@ -1,1 +1,24 @@
-console.log("Test");
+'use strict';
+
+$(document).ready(init);
+
+function init(){
+  // $('.save').click(saveImage);
+  // $('.delete').click(deleteImage);
+}
+
+var socket = io.connect('http://knockknockwhosthere.herokuapp.com');
+socket.on('photo', function (data) {
+  console.log(data);
+  $('.image').attr('src', data.img);
+  socket.emit('my other event', { my: 'data' });
+});
+
+// function saveImage(){
+
+// }
+
+// function delete(){
+
+// }
+
